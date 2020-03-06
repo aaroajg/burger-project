afdefb1f68ba111b290f798b34d9217aee759ebb
Aarohi <aarohi.ajgaonkar@active.ai>
2020-03-06 15:38:12 +0530

    Buttons added and Order summary completed

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -76,6 +76,10 @@ class BurgerBuilder extends Component {
         this.setState({ purchasing: false })
     }
 
+    purchaseContinueHandler = () => {
+        alert('You continued!')
+    }
+
     render() {
         const disabledInfo = { ...this.state.ingredients }
         for(let key in disabledInfo) {
@@ -85,7 +89,12 @@ class BurgerBuilder extends Component {
         return (
             <Aux>
                 <Modal show={this.state.purchasing} modalClosed={this.purchaseCloseHandler}>
-                    <OrderSummary ingredients={this.state.ingredients} />
+                    <OrderSummary 
+                        ingredients={this.state.ingredients} 
+                        cancel={this.purchaseCloseHandler} 
+                        continue={this.purchaseContinueHandler} 
+                        price={this.state.totalPrice}
+                    />
                 </Modal>
                 <Burger ingredients={this.state.ingredients} />
                 <BuildControls 
